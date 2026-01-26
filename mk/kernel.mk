facb10e94139820475307ff92c29f8196a5765ee
IPOleksenko <1ipooip1@gmail.com>
2026-01-26 03:22:25 +0000

    add LIBGCC variable for automatic libgcc linking

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -7,5 +7,5 @@ $(BUILD)/kernel32.o: src/kernel/kernel32.c
 	gcc -m32 -ffreestanding -fno-builtin -nostdinc -Ilib/h -c $< -o $@
 
 $(KERNEL_BIN): $(BUILD)/entry32.o $(BUILD)/kernel32.o $(LIB_OBJS)
-	ld -m elf_i386 -T src/kernel/linker.ld -nostdlib --oformat elf32-i386 -o $(KERNEL_ELF) $^
+	ld -m elf_i386 -T src/kernel/linker.ld -nostdlib --oformat elf32-i386 -o $(KERNEL_ELF) $^ $(LIBGCC)
 	$(OBJCOPY) -O binary $(KERNEL_ELF) $@
